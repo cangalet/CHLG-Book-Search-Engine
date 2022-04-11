179dc508bc2f329381afa97997ad0ab8b46a3442
Chris A <cangalet@gmail.com>
2022-04-10 19:12:44 -0700

    re-push files to repo

--- server/server.js
+++ server/server.js
@@ -1,24 +1,10 @@
 const express = require('express');
 const path = require('path');
-// import apollo server
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-
-// import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-// db connection
 const db = require('./config/connection');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
-
-server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -28,13 +14,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
